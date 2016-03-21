63809f3f72ff02b2e30bbd631acadbe1996e8d85
LikeSetaria <zbc_210@163.com>
2016-03-21 20:48:01 +0800

    Bigram going on

--- src/cn/edu/whu/Bigram.java
+++ src/cn/edu/whu/Bigram.java
@@ -3,10 +3,12 @@
  */
 package cn.edu.whu;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Queue;
-
+import org.apache.commons.io.FileUtils;
 /**
  * @author bczhang
  *
@@ -15,14 +17,32 @@ public class Bigram {
 	
 	
 	public static void main(String[] args){
-		Map<Integer,String > gramMap=new HashMap<Integer,String>();
-		String[] st=formGrams("那些年我们发过的微博网络营销少帅严刚",2);
-		for(int i=0;i<st.length;i++){
-	    System.out.println("the length is "+st.length+"    "+st[i]);
-	    }
+		
+//		 System.out.println(makeNgrams(",那,些sfgds,sgdgsd,那些那些那些那些那些年我们发过的微博网络营销少帅严刚",2));
+		File file =new File("D:\\DOWNLOAD\\BaiduYunDownload\\Major\\weibo\\RESULT\\weibo_users_name.txt");
+		String content="";
+		 try {
+			content=FileUtils.readFileToString(file);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.print( makeNgrams(content,2));
 	}
-    public  void makeNgrams(String  text,int nGramSize) {
+    public  static HashMap<String,Integer> makeNgrams(String  text,int nGramSize) {
+    	HashMap<String,Integer > gramMap=new HashMap<String,Integer>();
+    	String[] str=formGrams(text,nGramSize);
     	 
+    	for(String s:str){
+    		Integer cou;
+    		if(s.matches("^[a-zA-Z0-9\u4e00-\u9fa5]+$")){
+    		cou=gramMap.get(s);
+    		 gramMap.put(s, cou == null?1:cou+1);
+    		}
+    		
+    	}
+    	return   gramMap;
+    	
 }
     /**
      * Bigram 切割词
