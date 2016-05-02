76909a4f2689d9c3e2907d3863c0609770413868
LikeSetaria <zbc_210@163.com>
2016-05-02 19:37:57 +0800

    特征抽取

--- src/cn/edu/whu/spam/ExtractTrait.java
+++ src/cn/edu/whu/spam/ExtractTrait.java
@@ -6,6 +6,9 @@ package cn.edu.whu.spam;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -14,6 +17,8 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.LineIterator;
@@ -38,13 +43,15 @@ public class ExtractTrait {
 		//抽取部分用户的profile信息
 		//ext.extractItemByUID("D:/Whuer/FudanData/weibo_users.txt", UID_FILE_PATH , "E:/spam/UserItems.txt");
 		//抽取微博
-		ext.extractWeibo("D:/Whuer/FudanData/MERGE","E:/spam/weibo");
-		ext.save("E:/spam/weibo");
-		for (Entry<String, StringBuilder> entry : USER_WEIBO_MAP.entrySet()) {  
-			  
-		    System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());  
-		  
-		}  
+//		ext.extractWeibo("D:/Whuer/FudanData/MERGE","E:/spam/weibo");
+//		ext.save("E:/spam/weibo");
+//		for (Entry<String, StringBuilder> entry : USER_WEIBO_MAP.entrySet()) {  
+//			  
+//		    System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());  
+//		  
+//		} 
+		//提取特征
+		ext.extractFeatures("E:/spam/weibo/159597830.txt");
 	 
 	}
 	/**
@@ -168,7 +175,7 @@ public class ExtractTrait {
     //每个用户的微博保存为一个文件
     public void save(String saveFolderPath){
         //File saveFile=new File(saveFolderPath);
-     	 StringBuffer str=null;;
+     	 StringBuffer str=null;
      	 int cou=0;
         Iterator<?> iter =USER_WEIBO_MAP.entrySet().iterator();
         while(iter.hasNext()){
@@ -189,6 +196,69 @@ public class ExtractTrait {
         }
         
     }
- 
+    /**
+     * 提取一个用户的特征，
+     * 输入：一个用户的所有微博(一个uid命名的txt文件)
+     * 结构: userid,reposts_count,comment_count,source,created_at, text
+	         解释: 用户ID,转发数,评论数,来源,创建时间,文本内容
+	         @param the URL of user 
+	         @return 返回一个字符串，UID，是否来自移动终端 ，微博中@数量，微博中URL
+     */
+    //采取新的规范的文件处理方式
+    public String extractFeatures(String URL){
+    	Path path=Paths.get(URL);
+    	//微博中@数量
+    	int atNums=0;
+    	//微博中URL数量
+    	int URLNums=0;
+    	//微博中的是否提及话题，#topic#
+    	int topicNums=0;
+    	//微博来源，移动终端为0，非移动终端为1。具体实现，计数比较，如果移动终端微博数大于非移动终端则为0
+    	int MCount,nonMCount;
+    	//转发数
+    	int reposts_count=0;
+    	//评论数
+    	int comment_count=0;
+    	String content;
+    	 
+    	  try {
+			 content = new String(Files.readAllBytes(path));
+			 Pattern pattern = Pattern.compile("//(//d{3}//)//s//d{3}-//d{4}");
+			
+			String[]  strb=content.split("\\n");
+			String[] temp=null;
+			
+			System.out.println("total Nums Of weibo "+strb.length);
+			for(String ss:strb){
+				StringBuilder tem=new StringBuilder();
+				String[] arr=ss.split(",");
+				//存储规范的数组切割
+				String[] arr2=new String[6];
+				//复制数组前5个，这部分是格式化的，第6才可能出现异常
+				for(int i=0;i<6;i++)
+					arr2[i]=arr[i];
+				//原文本以逗号为分割，但是可能text中含有英文逗号，所以对于这些特殊的需要规范化处理
+				if(arr.length>6){
+					for(int i=5;i<arr.length;i++){
+						tem.append(arr[i]);
+					}
+					arr2[5]=tem.toString();
+				}
+				//System.out.println(arr2[5]);
+				reposts_count=Integer.valueOf(arr2[1])+reposts_count;
+				comment_count=Integer.valueOf(arr2[2])+comment_count;
+				 
+			}
+			System.out.println("评论数"+comment_count+"转发数"+reposts_count);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}  
+    	  return null;
+    	  
+         
+    }
+    
 
 }
